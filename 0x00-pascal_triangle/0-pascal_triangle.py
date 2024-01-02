1f152d6dbbabe9cd4d956ab3172f2415036e8e12
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 00:12:34 +0200

    pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""Pascal's Triangle"""
+def pascal_triangle(n):
+    triangle = [[1]]
+
+    for i in range(1, n):
+        temp = []
+        row = [0] + triangle[-1] + [0]
+        for j in range(i + 1):
+            temp.append(row[j] + row[j + 1])
+        triangle.append(temp)
+    return triangle
---
+++ 0x00-pascal_triangle/map.md
@@ -0,0 +1,11 @@
+# n = 5
+
+tri = [[1]]
+
+loop:
+
+i = 0, row = [0, 1, 0], loop : j = 0 - 1, temp = [1, 1]
+triangle = [[1], [1, 1]]
+
+i = 1, row = [0, 1, 1, 0], loop : temp = [1, 2, 1]
+triangle = [[1], [1, 1], [1, 2, 1]]

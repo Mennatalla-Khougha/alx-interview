1596e4a9458f8cc8d9b989ef684788e8abfa3548
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-23 19:33:54 +0200

    tasks 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -3,28 +3,28 @@
 import sys
 
 
+def print_output(stats, size):
+    """
+    The function `print_output` prints the file size and the statistics in
+    a formatted manner.
+
+    :param stats: A dictionary containing statistics about a file. The keys
+    of the dictionary represent the type of statistic and the values represent
+    the corresponding count of that statistic
+    :param size: The `size` parameter represents the file size. It is a value
+    that indicates the size of a file in bytes
+    """
+    print(f"File size: {size}")
+
+    for i in sorted(stats):
+        if stats[i]:
+            print(f"{i}: {stats[i]}")
+
+
 if __name__ == "__main__":
     count = 0
     stats = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
     size = 0
-
-    def print_output():
-        """
-        The function `print_output` prints the file size and the statistics in
-        a formatted manner.
-
-        :param stats: A dictionary containing statistics about a file. The keys
-        of the dictionary represent the type of statistic and the values represent
-        the corresponding count of that statistic
-        :param size: The `size` parameter represents the file size. It is a value
-        that indicates the size of a file in bytes
-        """
-        print(f"File size: {size}")
-
-        for i in sorted(stats):
-            if stats[i]:
-                print(f"{i}: {stats[i]}")
-
 
     try:
         for line in sys.stdin:
@@ -39,7 +39,7 @@ if __name__ == "__main__":
             stats[code] += 1
 
             if count % 10 == 0:
-                print_output()
-        print_output()
+                print_output(stats, size)
+        print_output(stats, size)
     except KeyboardInterrupt:
-        print_output()
+        print_output(stats, size)

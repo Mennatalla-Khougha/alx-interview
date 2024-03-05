6fc47417d7e97a9317a5bd2cc0cf01c9ddc824f0
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-06 00:36:39 +0200

    task 0

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -11,21 +11,9 @@ def island_perimeter(grid):
             if grid[r][c] == 1:
                 dirct = [(r - 1, c), (r, c - 1), (r + 1, c), (r, c + 1)]
                 for dr, dc in dirct:
-                    if -1 < dr < rows and -1 < dc < cols and grid[dr][dc] == 0:
+                    if dr == -1 or dr == rows or dc == -1 or dc == cols:
+                        size += 1
+                    elif grid[dr][dc] == 0:
                         size += 1
 
     return size
-    # width = len(grid[0])
-    # height = len(grid)
-    # edges = 0
-    # size = 0
-
-    # for i in range(height):
-    #     for j in range(width):
-    #         if grid[i][j] == 1:
-    #             size += 1
-    #             if (j > 0 and grid[i][j - 1] == 1):
-    #                 edges += 1
-    #             if (i > 0 and grid[i - 1][j] == 1):
-    #                 edges += 1
-    # return size * 4 - edges * 2
